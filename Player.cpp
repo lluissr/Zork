be767ee97d2c9895b31b3b1f861b6acbb79bee64
lluissr <lluissr@gmail.com>
2018-03-11 15:58:32 +0100

    Refactor in Player PerformAction method
    - Now every Player action has is own private method. PerformAction
    method calls the correct private method

--- Player.cpp
+++ Player.cpp
@@ -14,150 +14,196 @@ void Player::PerformAction(vector<string>& action)
 	//Look something
 	if (action[0] == "look")
 	{
-		m_Location->Look();
+		Look(action);
 	}
 	//Go in a specific direction
 	else if (action[0] == "go" || action[0] == "north" || action[0] == "east" || action[0] == "west" || action[0] == "south")
 	{
-		if (action[0] == "go" && action.size() > 1)
-		{
-			action[0] = action[1];
-		}
-
-		if (action[0] == "north" || action[0] == "east" || action[0] == "west" || action[0] == "south")
-		{
-
-			Exit* exit = m_Location->GetExit(action[0]);
-
-			if (exit == NULL)
-			{
-				cout << "There is no exit at " << action[0];
-			}
-			else
-			{
-				m_Location = exit->GetDestinationRoom(m_Location);
-				cout << "You take the exit and you get to the " << m_Location->GetName() << "\n";
-				m_Location->Look();
-			}
-		}
-		else
-		{
-			cout << "This is not a valid direction.\n";
-		}
+		Go(action);
 	}
 	//Get Item
 	else if ((action[0] == "take" || action[0] == "get") && action.size() > 1)
 	{
-		Entity* item = m_Location->GetItem(action[1], true);
-
-		if (item == NULL)
-		{
-			cout << "There is not " << action[1] << ".\n";
-		}
-		else
-		{
-			m_Contains.push_back(item);
-			cout << "Taken.\n";
-		}
+		Take(action);
 	}
 	//Drop Item
 	else if ((action[0] == "drop" || action[0] == "release") && action.size() > 1)
 	{
-		Entity* item = GetItem(action[1]);
-		if (item == NULL)
-		{
-			cout << "You don't have " << action[1] << " in your inventory.\n";
-		}
-		else
-		{
-			m_Location->AddEntity(item);
-			cout << "Dropped.\n";
-		}
+		Drop(action);
 	}
 	//Check inventory
 	else if (action[0] == "inventory")
 	{
-		if (m_Contains.size() == 0)
-		{
-			cout << "Your inventory is empty";
-		}
-		else
-		{
-			cout << "You are carrying:";
-			for each (Entity* entity in m_Contains)
-			{
-				if (entity->GetType() == ITEM)
-				{
-					cout << "\n" << entity->GetName() << ": " << entity->GetDescription();
-				}
-			}
-		}
+		Inventory(action);
 	}
 	//Open Objects
 	else if (action[0] == "open" && action.size() > 1)
 	{
-		bool found = false;
-		for each (Entity* entity in m_Contains)
-		{
-			if (entity->GetType() == ITEM)
-			{
-				string itemName = entity->GetName();
-				if (itemName == action[1])
-				{
-					Item* item = (Item*)entity;
-					item->Open();
-					found = true;
-				}
-			}
-		}
-
-		Entity* roomEntity = m_Location->GetItem(action[1], false);
-		if (roomEntity != NULL)
-		{
-			Item* roomItem = (Item*)roomEntity;
-			roomItem->Open();
-			found = true;
-		}
-
-		if (!found)
-		{
-			cout << "There is not " << action[1] << " in the room or in your inventory.";
-		}
+		Open(action);
 	}
 	//Close Objects
 	else if (action[0] == "close" && action.size() > 1)
 	{
-		bool found = false;
-		for each (Entity* entity in m_Contains)
-		{
-			if (entity->GetType() == ITEM)
-			{
-				string itemName = entity->GetName();
-				if (itemName == action[1])
-				{
-					Item* item = (Item*)entity;
-					item->Close();
-					found = true;
-				}
-			}
-		}
+		Close(action);
+	}
+	//Incorrect action
+	else
+	{
+		cout << "Incorrect command\n";
+	}
+}
 
-		Entity* roomEntity = m_Location->GetItem(action[1], false);
-		if (roomEntity != NULL)
-		{
-			Item* roomItem = (Item*)roomEntity;
-			roomItem->Close();
-			found = true;
-		}
 
-		if (!found)
+//--Private Methods
+
+
+void Player::Look(vector<string>& action)
+{
+	m_Location->Look();
+}
+
+
+void Player::Go(vector<string>& action)
+{
+	if (action[0] == "go" && action.size() > 1)
+	{
+		action[0] = action[1];
+	}
+
+	if (action[0] == "north" || action[0] == "east" || action[0] == "west" || action[0] == "south")
+	{
+
+		Exit* exit = m_Location->GetExit(action[0]);
+
+		if (exit == NULL)
 		{
-			cout << "There is not " << action[1] << " in the room or in your inventory.";
+			cout << "There is no exit at " << action[0];
+		}
+		else
+		{
+			m_Location = exit->GetDestinationRoom(m_Location);
+			cout << "You take the exit and you get to the " << m_Location->GetName() << "\n";
+			m_Location->Look();
 		}
 	}
 	else
 	{
-		cout << "Incorrect command\n";
+		cout << "This is not a valid direction.\n";
+	}
+}
+
+
+void Player::Take(vector<string>& action)
+{
+	Entity* item = m_Location->GetItem(action[1], true);
+
+	if (item == NULL)
+	{
+		cout << "There is not " << action[1] << ".\n";
+	}
+	else
+	{
+		m_Contains.push_back(item);
+		cout << "Taken.\n";
+	}
+}
+
+
+void Player::Drop(vector<string>& action)
+{
+	Entity* item = GetItem(action[1]);
+	if (item == NULL)
+	{
+		cout << "You don't have " << action[1] << " in your inventory.\n";
+	}
+	else
+	{
+		m_Location->AddEntity(item);
+		cout << "Dropped.\n";
+	}
+}
+
+
+void Player::Inventory(vector<string>& action)
+{
+	if (m_Contains.size() == 0)
+	{
+		cout << "Your inventory is empty";
+	}
+	else
+	{
+		cout << "You are carrying:";
+		for each (Entity* entity in m_Contains)
+		{
+			if (entity->GetType() == ITEM)
+			{
+				cout << "\n" << entity->GetName() << ": " << entity->GetDescription();
+			}
+		}
+	}
+}
+
+
+void Player::Open(vector<string>& action)
+{
+	bool found = false;
+	for each (Entity* entity in m_Contains)
+	{
+		if (entity->GetType() == ITEM)
+		{
+			string itemName = entity->GetName();
+			if (itemName == action[1])
+			{
+				Item* item = (Item*)entity;
+				item->Open();
+				found = true;
+			}
+		}
+	}
+
+	Entity* roomEntity = m_Location->GetItem(action[1], false);
+	if (roomEntity != NULL)
+	{
+		Item* roomItem = (Item*)roomEntity;
+		roomItem->Open();
+		found = true;
+	}
+
+	if (!found)
+	{
+		cout << "There is not " << action[1] << " in the room or in your inventory.";
+	}
+}
+
+
+void Player::Close(vector<string>& action)
+{
+	bool found = false;
+	for each (Entity* entity in m_Contains)
+	{
+		if (entity->GetType() == ITEM)
+		{
+			string itemName = entity->GetName();
+			if (itemName == action[1])
+			{
+				Item* item = (Item*)entity;
+				item->Close();
+				found = true;
+			}
+		}
+	}
+
+	Entity* roomEntity = m_Location->GetItem(action[1], false);
+	if (roomEntity != NULL)
+	{
+		Item* roomItem = (Item*)roomEntity;
+		roomItem->Close();
+		found = true;
+	}
+
+	if (!found)
+	{
+		cout << "There is not " << action[1] << " in the room or in your inventory.";
 	}
 }
 
--- Player.h
+++ Player.h
@@ -8,6 +8,13 @@ class Player : public Entity
 private:
 	Room* m_Location;
 	Entity* GetItem(string name);
+	void Look(vector<string>& action);
+	void Go(vector<string>& action);
+	void Take(vector<string>& action);
+	void Drop(vector<string>& action);
+	void Inventory(vector<string>& action);
+	void Open(vector<string>& action);
+	void Close(vector<string>& action);
 public:
 	Player(string name, string description, Room* room);
 	void PerformAction(vector<string>& action);
